2c19d24bf6adf90d9021386348db6351126f2c9c
Anjali Mishra <anjalimarch24@gmail.com>
2021-10-11 19:33:39 +0530

    committing changes

---
+++ test/specs/example.e2e.js
@@ -0,0 +1,13 @@
+
+const LoginPage = require('../pageobjects/login.page');
+const SecurePage = require('../pageobjects/secure.page');
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        
+//browser.url
+            
+    });
+});
+
+
---
+++ test/specs/pageObjects/loginPage.js
@@ -0,0 +1,41 @@
+const { default: $ } = require("webdriverio/build/commands/browser/$");
+
+class LoginPage{
+
+//userName=$("input[name='userName']")
+
+get userName(){
+
+    return $("input[name='userName']")
+
+}
+
+
+get password(){
+
+
+return $("input[name='password']")
+
+}
+
+get signIn(){
+
+
+
+return $("submit")
+
+
+}
+
+Login(userName,password){
+
+this.userName.setValue(userName)
+this.password.setValue(password)
+this.signIn.click();
+}
+
+
+}
+
+module.exports= new LoginPage()
+
